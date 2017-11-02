3db84b04b94ec80d148d4dc6573faadf08d8dde7
RMADE VMS <rmadevms@RMADEs-Mac-mini.local>
2017-11-02 19:57:26 +0530

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Hotspot, HotspotNetwork } from '@ionic-native/hotspot';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
+  data: any;
+  constructor(public navCtrl: NavController,private hotspot: Hotspot) {
 
   }
 
+  ionViewDidLoad(){
+    this.hotspot.scanWifi().then((networks: Array<HotspotNetwork>) => {
+
+      this.data=networks;
+      console.log(".........hotspot..........",JSON.stringify(networks));
+    });
+  }
 }
